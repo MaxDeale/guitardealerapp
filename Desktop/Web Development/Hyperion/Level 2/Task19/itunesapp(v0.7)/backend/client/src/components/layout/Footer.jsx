44920ce8a4e8e70f978af46b2f46e30c9faff894
MaxDeale <maxericdeale@gmail.com>
2020-02-06 17:54:52 +0200

    react-create-app on Heroku

---
+++ Desktop/Web Development/Hyperion/Level 2/Task19/itunesapp(v0.7)/backend/client/src/components/layout/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer id="mainFooter">
+        <p>&copy; Deale Development 2020</p>
+        <h6>All Rights Reserved.</h6>
+        <a href="https://www.dealedevelopment.com">
+          <img
+            style={{ width: "120px", height: "120px" }}
+            src="https://www.dealedevelopment.com/dist/img/DealeLogo.png"
+            alt=""
+          />
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
