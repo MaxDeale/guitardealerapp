230677b22d3e5ea43dfa47ac464ba56425bacb3d
MaxDeale <maxericdeale@gmail.com>
2020-04-13 14:52:12 +0200

    deploy

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
-
+//running saerver on port 5000
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
