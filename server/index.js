89772f229275cfea8759eb447d3d36c5e1c5061c
MaxDeale <maxericdeale@gmail.com>
2020-04-16 13:45:02 +0200

    more finishes

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 });
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+
   // Set static folder for client build folder
   app.use(express.static("client/build"));
 
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
-}
+
 //running saerver on port 5000
 const port = process.env.PORT || 5000;
 
